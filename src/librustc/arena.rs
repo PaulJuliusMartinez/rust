f8f8bf704167982d40346f1bd36400c1c400b122
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2018-11-30 20:15:09 +0100

    Update borrowck

--- src/librustc/arena.rs
+++ src/librustc/arena.rs
@@ -16,6 +16,7 @@ macro_rules! arena_types {
             )>,
             [few] mir_keys: rustc::util::nodemap::DefIdSet,
             [decode] specialization_graph: rustc::traits::specialization_graph::Graph,
+            [decode] borrowck: rustc::middle::borrowck::BorrowCheckResult,
         ], $tcx);
     )
 }
